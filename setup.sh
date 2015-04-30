ea9d1119f9b1a537a3781949b08611909123506d
Joshua Ziggas <zoshjiggas@gmail.com>
2015-04-29 20:59:59 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#TODO
+# ln -s ~/git/configs/vimrc ~/.vimrc
+# ln -s ~/git/configs/bashrc ~/.bashrc
+# ln -s ~/git/configs/gitconfig ~/.gitconfig
+# ln -s ~/git/configs/git-prompt.sh ~/.git-prompt.sh
